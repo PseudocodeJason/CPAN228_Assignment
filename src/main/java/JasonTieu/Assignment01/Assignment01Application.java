b7ed81e9326527c9d25298a6a3814a928b83b459
Jason <jason.tieu@Hotmail.com>
2023-02-19 22:23:44 -0500

    Add data when starting website

--- src/main/java/JasonTieu/Assignment01/Assignment01Application.java
+++ src/main/java/JasonTieu/Assignment01/Assignment01Application.java
@@ -1,13 +1,30 @@
 package JasonTieu.Assignment01;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import JasonTieu.Assignment01.model.Item;
+import JasonTieu.Assignment01.model.Item.Brand;
+import JasonTieu.Assignment01.repository.ItemRepositroy;
 
 @SpringBootApplication
 public class Assignment01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment01Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner dataLoader(ItemRepositroy repositroy){
+		return args ->{
+			repositroy.save(Item.builder()
+			.name("Bag")
+			.brandForm(Brand.GUCCI)
+			.yearOfCreation(2022)
+			.price(2500.00).build());
+		};
 	}
 
 }
