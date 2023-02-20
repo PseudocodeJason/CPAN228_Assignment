0545c2109c460c78eba2c111346a238a86c23a52
Jason <jason.tieu@Hotmail.com>
2023-02-19 22:24:19 -0500

    Showing items in store

--- src/main/java/JasonTieu/Assignment01/controller/HomeController.java
+++ src/main/java/JasonTieu/Assignment01/controller/HomeController.java
@@ -1,14 +1,27 @@
 package JasonTieu.Assignment01.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import JasonTieu.Assignment01.repository.ItemRepositroy;
 
 @Controller
 public class HomeController {
+    private ItemRepositroy itemRepositroy;
+
+    public HomeController(ItemRepositroy itemRepositroy){
+        this.itemRepositroy = itemRepositroy;
+    }
     
     @GetMapping("/")
     public String home(){
         return "Homepage";
     }
 
+    @ModelAttribute
+    public void items(Model model){
+        model.addAttribute("items", itemRepositroy.findAll());
+    }
 }
